04c3c06c14265a419580eace743bbe95e273316d
Braeden Bucher <130403961+braedenbucher@users.noreply.github.com>
2025-03-05 11:44:36 -0600

    Complete Refactor

--- Textures.java
+++ Textures.java
@@ -1,6 +1,4 @@
 public class Textures {
-    // all textures are 5x4
-
     public static final char[][] default_texture = {
                 {'X', '_', '_', '_', 'X', 'X'},
                 {'|', '\\', '_', '_', '\\', 'X'},
